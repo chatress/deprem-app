9670fd6e03a4b9706197ab56f82ddf286fa7636c
Sertaç Aydoğdu <32239887+chatress@users.noreply.github.com>
2025-05-01 03:55:53 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// 4+ büyüklükteki depremleri göster
-L.geoJSON(earthquakes, {
-    filter: feature => feature.properties.mag >= 4
-}).addTo(map);
-
-const redIcon = L.icon({iconUrl: 'red-marker.png'});
-if (city.risk > 7) marker.setIcon(redIcon);
-
