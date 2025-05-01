fc8b5aab67461493f5845e192edfb3664ff6ddb6
Sertaç Aydoğdu <32239887+chatress@users.noreply.github.com>
2025-05-01 03:38:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// app.js
-fetch('https://api.orhanaydogdu.com.tr/deprem/kandilli/live')
-    .then(response => response.json())
-    .then(data => {
-        data.result.forEach(deprem => {
-            document.getElementById('depremler').innerHTML += `
-                <div class="deprem">
-                    <strong>${deprem.title}</strong> - Büyüklük: ${deprem.mag}
-                </div>
-            `;
-        });
-    });
+// 4+ büyüklükteki depremleri göster
+L.geoJSON(earthquakes, {
+    filter: feature => feature.properties.mag >= 4
+}).addTo(map);
+
+const redIcon = L.icon({iconUrl: 'red-marker.png'});
+if (city.risk > 7) marker.setIcon(redIcon);
+
